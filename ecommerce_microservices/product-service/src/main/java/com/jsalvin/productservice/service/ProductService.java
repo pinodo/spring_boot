bd042a8a0f1c979c69e86ba92e9c7bb0ba4d5e7f
alvinkim <palifarec@gmail.com>
2023-01-23 20:32:01 -0800

    initiate product-service

---
+++ ecommerce_microservices/product-service/src/main/java/com/jsalvin/productservice/service/ProductService.java
@@ -0,0 +1,27 @@
+package com.jsalvin.productservice.service;
+
+import com.jsalvin.productservice.dto.ProductRequest;
+import com.jsalvin.productservice.model.Product;
+import com.jsalvin.productservice.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public void createProduct(ProductRequest productRequest) {
+        Product product = Product.builder()
+                .name(productRequest.getName())
+                .description(productRequest.getDescription())
+                .price(productRequest.getPrice())
+                .build();
+
+        productRepository.save(product);
+        log.info("Product {} is saved", product.getId());
+    }
+}

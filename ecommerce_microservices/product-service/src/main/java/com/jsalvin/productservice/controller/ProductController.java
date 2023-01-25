feacea33fa241a8712bc5692c168ac6a6fa6856d
Alvin <palifarec@gmail.com>
2023-01-24 20:29:39 -0800

    feat: add test that product is created
    bug: java.lang.IllegalStateException: Could not find a valid Docker environment.

--- ecommerce_microservices/product-service/src/main/java/com/jsalvin/productservice/controller/ProductController.java
+++ ecommerce_microservices/product-service/src/main/java/com/jsalvin/productservice/controller/ProductController.java
@@ -1,10 +1,13 @@
 package com.jsalvin.productservice.controller;
 
 import com.jsalvin.productservice.dto.ProductRequest;
+import com.jsalvin.productservice.dto.ProductResponse;
 import com.jsalvin.productservice.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/product")
@@ -17,5 +20,11 @@ public class ProductController {
     @ResponseStatus(HttpStatus.CREATED)
     public void createProduct(@RequestBody ProductRequest productRequest) {
         productService.createProduct(productRequest);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<ProductResponse> getAllProducts() {
+        return productService.getAllProducts();
     }
 }
--- ecommerce_microservices/product-service/src/main/java/com/jsalvin/productservice/service/ProductService.java
+++ ecommerce_microservices/product-service/src/main/java/com/jsalvin/productservice/service/ProductService.java
@@ -1,11 +1,14 @@
 package com.jsalvin.productservice.service;
 
 import com.jsalvin.productservice.dto.ProductRequest;
+import com.jsalvin.productservice.dto.ProductResponse;
 import com.jsalvin.productservice.model.Product;
 import com.jsalvin.productservice.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -23,5 +26,20 @@ public class ProductService {
 
         productRepository.save(product);
         log.info("Product {} is saved", product.getId());
+    }
+
+    public List<ProductResponse> getAllProducts() {
+        List<Product> products = productRepository.findAll();
+
+        return products.stream().map(this::mapToProductResponse).toList();
+    }
+
+    private ProductResponse mapToProductResponse(Product product) {
+        return ProductResponse.builder()
+                .id(product.getId())
+                .name(product.getName())
+                .description(product.getDescription())
+                .price(product.getPrice())
+                .build();
     }
 }

44352d9ed1b38102333c2957c5f9f53270efb299
alvinkim <palifarec@gmail.com>
2023-01-27 16:54:43 -0800

    feat: add spring_boot_docker_test

---
+++ spring_boot_docker_test/docker-demo/src/main/java/com/jsalvin/dockerdemo/controller/MyController.java
@@ -0,0 +1,13 @@
+package com.jsalvin.dockerdemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+
+    @GetMapping("/test-docker")
+    public String getData() {
+        return "in docker tutorial project";
+    }
+}

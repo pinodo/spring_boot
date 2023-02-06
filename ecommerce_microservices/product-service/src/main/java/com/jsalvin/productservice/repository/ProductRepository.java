7931be264d0d385fc1906c0daf0ae24ea49d412f
Alvin Kim <56916613+pinodo@users.noreply.github.com>
2023-02-05 21:46:39 -0800

    Delete ecommerce_microservices directory

--- ecommerce_microservices/product-service/src/main/java/com/jsalvin/productservice/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.jsalvin.productservice.repository;
-
-import com.jsalvin.productservice.model.Product;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ProductRepository extends MongoRepository<Product, String> {
-}
